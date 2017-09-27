b6f724e8c6911534f9a088452d8d67a6b9337ef7
AyselAydin <ayselaydin01@gmail.com>
2017-09-27 23:25:07 +0300

    Aysel
    telefonun fotoğraflar içeriğine erişme işlemi yapıldıç

--- app/src/main/java/com/example/aysel/firebaseblog/PostActivity.java
+++ app/src/main/java/com/example/aysel/firebaseblog/PostActivity.java
@@ -1,13 +1,42 @@
 package com.example.aysel.firebaseblog;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
-public class PostActivity extends AppCompatActivity {
+public class PostActivity extends AppCompatActivity implements View.OnClickListener {
 
+    private ImageButton mImgSelect;
+    private static final int GALLERY_REQUEST = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
+
+        mImgSelect = findViewById(R.id.imgSelect);
+
+        mImgSelect.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        if(view == mImgSelect){
+            Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
+            galleryIntent.setType("image/*");
+            startActivityForResult(galleryIntent, GALLERY_REQUEST);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK){
+            Uri imageUri = data.getData();
+            mImgSelect.setImageURI(imageUri);
+        }
     }
 }
