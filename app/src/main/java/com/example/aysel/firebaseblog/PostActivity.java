45c302cb9e0a700a57e5981e8793ed8cb5984c9f
AyselAydin <ayselaydin01@gmail.com>
2017-09-28 00:51:12 +0300

    Aysel
    Add new post

--- app/src/main/java/com/example/aysel/firebaseblog/PostActivity.java
+++ app/src/main/java/com/example/aysel/firebaseblog/PostActivity.java
@@ -1,42 +1,97 @@
 package com.example.aysel.firebaseblog;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageButton;
+
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
 public class PostActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageButton mImgSelect;
+    private EditText mPostTitle, mPostDesc;
+    private Button mSubmit;
+    private Uri mimageUri = null;
+    private StorageReference mStorage;
+    private DatabaseReference mReference;
+    private ProgressDialog mProgress;
     private static final int GALLERY_REQUEST = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_post);
 
-        mImgSelect = findViewById(R.id.imgSelect);
+        mStorage = FirebaseStorage.getInstance().getReference();
+        mReference = FirebaseDatabase.getInstance().getReference().child("Blog");
 
+        mProgress = new ProgressDialog(this);
+        mPostTitle = findViewById(R.id.titleField);
+        mPostDesc = findViewById(R.id.descField);
+        mSubmit = findViewById(R.id.btn_submitPost);
+        mSubmit.setOnClickListener(this);
+        mImgSelect = findViewById(R.id.imgSelect);
         mImgSelect.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
-        if(view == mImgSelect){
+        if (view == mImgSelect) {
             Intent galleryIntent = new Intent(Intent.ACTION_GET_CONTENT);
             galleryIntent.setType("image/*");
             startActivityForResult(galleryIntent, GALLERY_REQUEST);
+        } else if (view == mSubmit) {
+            startPosting();
         }
+    }
+
+    private void startPosting() {
+        mProgress.setMessage("Posting to Blog..");
+        mProgress.show();
+
+        final String title_val = mPostTitle.getText().toString().trim();
+        final String desc_val = mPostDesc.getText().toString().trim();
+
+        if (!TextUtils.isEmpty(title_val) && !TextUtils.isEmpty(desc_val) && mimageUri != null) {
+            StorageReference filepath = mStorage.child("Blog_Images").child(mimageUri.getLastPathSegment());
+            filepath.putFile(mimageUri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                @Override
+                public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                    Uri downloadUrl = taskSnapshot.getDownloadUrl();
+
+                    DatabaseReference newPost = mReference.push();
+                    newPost.child("title").setValue(title_val);
+                    newPost.child("desc").setValue(desc_val);
+                    newPost.child("image").setValue(downloadUrl.toString());
+
+                    mProgress.dismiss();
+                    startActivity(new Intent(PostActivity.this, MainActivity.class));
+                }
+            });
+        }
+        else
+            mProgress.dismiss();
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
 
-        if(requestCode == GALLERY_REQUEST && resultCode == RESULT_OK){
-            Uri imageUri = data.getData();
-            mImgSelect.setImageURI(imageUri);
+        if (requestCode == GALLERY_REQUEST && resultCode == RESULT_OK) {
+            mimageUri = data.getData();
+            mImgSelect.setImageURI(mimageUri);
         }
     }
 }
